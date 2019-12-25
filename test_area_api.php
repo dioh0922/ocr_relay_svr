ba4086232bf2551da06d8b5d30015eeaf560b523
dioh0922 <43507423+dioh0922@users.noreply.github.com>
2019-12-25 10:54:28 +0900

    領域をブラウザで選択するように変更

--- test_area_api.php
+++ test_area_api.php
@@ -4,8 +4,38 @@
 	//POSTしてきた画像でOCRするように変更する
 
 	//ファイル名は引数で入力するように変更する
+	$file_name = $_FILES["upload_img"]["tmp_name"];
 
-	foreach(getallheaders() as $name => $val){
-		echo "$name: $val\n";
+	if(!is_uploaded_file($file_name)){
+		print("画像ファイルが以上です");
+		exit();
 	}
+
+	$tmp_img_name = "tmp.jpg";
+
+	if(file_exists($tmp_img_name)){
+		unlink($tmp_img_name);
+	}
+
+	move_uploaded_file($file_name, $file_name);
+
+	list($width, $height) = getimagesize($file_name);
+
+	$row_image = imagecreatefromjpeg($file_name);
+
+
+	$resize_w = (int)($width);
+	$resize_h = (int)($height);
+
+	$image = imagecreatetruecolor($resize_w, $resize_h);
+
+	imagecopyresampled($image, $row_image, 0, 0, 0, 0, $resize_w, $resize_h, $width, $height);
+
+	$id = mb_substr($file_name, -8, 4);
+
+	imagejpeg($image, $tmp_img_name);
+	//いつさくじょするか？
+
+	echo $id;
+
 ?>
--- title_ocr.js
+++ title_ocr.js
@@ -15,84 +15,68 @@ var control_file_select = new Vue({
 	}
 });
 
+var crop_obj = null;
+
 function load_local_Image(e){
-	var file = e.target.files[0];
+	let file = e.target.files[0];
 	if(!file.type.match("image.*")){
 		alert("画像でない");
 		return;
 	}
 
-	var blob = window.URL.createObjectURL(file);
-	$("#resultImg").empty();
+	$("#send_crop_btn").hide();
+
+	control_result.text = "タイトル部分を選択してください";
+
 	$("<img>").attr({
-		src: blob,
+		src: "",
 		id: "prev_src",
 		alt: "",
-		style: "margin-bottom: 20px; width:50%; height:50%;"
-	}).appendTo("#img_preview");
-	control_result.text = "判別しています";
-	//get_img_title();
+	}).prependTo("#img_preview");
 
-	var formdata = new FormData($("#img_form").get(0));
+	$("#send_crop_btn").show();
 
-	$.ajax({
-		type: "POST",
-		url: svr + "test_ather_post.php",
-		cacha:false,
-		contentType: false,
-		processData: false,
-		data: formdata,
-		dataType: "html"
-	})
-	.done(function(ajax_data){
-		console.log(ajax_data);
-		control_result.text = ajax_data;
-	})
-	.fail(function(){
-		control_result.text = "OCRサーバへの通信が失敗しました。";
-	});
-}
 
-function get_all_area_img(cnt){
-	for(let i = 0; i < cnt; i++){
+	let formdata = new FormData($("#img_form").get(0));
+	let src = window.URL.createObjectURL(file);
 
-		let src = svr + "area/" + i + ".jpg";
-		let id = "img" + i;
-		$("<img ><br>").attr({
-			src: src,
-			id: id,
-			alt: "",
-			name: i
-		}).appendTo("#resultImg");
-		$("#"+id).click(img_click);
+	if(crop_obj != null){
+		crop_obj.destroy();
 	}
 
-	//領域画像変更のイベントを登録しておき、文字識別処理を呼ばせる
-	$(document).on("img_select", img_select);
-}
-
-function img_select(e, txt){
-	let post = {
-		"target" : txt
-	};
-
-	$.ajax({
-		type: "POST",
-		url: svr + "get_title_api.php",
-		cacha:false,
-		data: post,
-	})
-	.done(function(ajax_data){
-		control_result.text = ajax_data + "番の画像を抽出します";
-	})
-	.fail(function(){
-		control_result.text = "OCRサーバへの通信が失敗しました。";
+	$("#prev_src").attr("src", src);
+	$("#prev_src").ready(function(){
+		let el = $("#prev_src").get(0);
+		crop_obj = new Cropper(el,{
+			cropend(event){
+			}
+		});
 	});
 }
 
-function img_click(){
-		$(document).trigger("img_select", [this.name]);
-		$("#resultImg").empty();
+function send_crop_img_to_api(){
+	control_result.text = "識別しています";
+
+	crop_obj.getCroppedCanvas().toBlob((blob) => {
+		let formdata = new FormData();
+		formdata.append("upload_img", blob);
+		$.ajax({
+			type: "POST",
+			url: svr + "get_area_api.php",
+			cacha:false,
+			contentType: false,
+			processData: false,
+			data: formdata,
+			dataType: "html"
+		})
+		.done(function(ajax_data){
+			//現状はOCRサーバでは文字列の抽出のみ行うため、結果表示のみ
+			control_result.text = ajax_data;
+		})
+		.fail(function(){
+			control_result.text = "OCRサーバへの通信が失敗しました。";
+		});
+	}, "image/jpeg"); //jpeg形式にする(toBlob()そのままだとpngになる)
 }
 
 function get_img_title(){
@@ -109,45 +93,13 @@ function get_img_title(){
 		dataType: "html"
 	})
 	.done(function(ajax_data){
-		let str = ajax_data.slice(0, -1);
-		str = str.split('\'').join("")
-		str = "[" + str + "]";
-		let json = JSON.parse(str);
-
-		control_result.text = json.length + "個の文字列を抽出しました";
-
-		prev_area_img(json);
+		//現状はOCRサーバでは文字列の抽出のみ行うため、結果表示のみ
+		control_result.text = ajax_data;
 	})
 	.fail(function(){
 		control_result.text = "OCRサーバへの通信が失敗しました。";
 	});
 
-}
-
-function prev_area_img(pos_arr){
-	let src = $("#img_preview").children("img").attr("src");
-	let cvs = document.getElementById("img_canvas");
-	let ctx = cvs.getContext("2d");
-	let img = new Image();
-	img.src = src;
-	img.onload = function(){
-		let xs = pos_arr[1].xs;
-		let ys = pos_arr[1].ys;
-		let xe = pos_arr[1].xe;
-		let ye = pos_arr[1].ye;
-		let dw = $(".canvas_wrapper").width();
-		//cvs.width = dw;
-		//cvs.height = dw;
-		//ctx.drawImage(img, xs, ys, xe, ye, 0, 0, cvs.width, cvs.height);
-		ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, cvs.width, cvs.height);
-		let pr_img = ctx.getImageData(0, 0, img.width, img.height / 2);
-		ctx.putImageData(pr_img, 0, 0)
-	}
-}
-
-function part_result_from_array(result){
-	var arr = result.split(",");
-	console.log(arr);
 }
 
 //起動時の処理
