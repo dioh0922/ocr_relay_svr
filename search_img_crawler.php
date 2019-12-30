6f4320ee14eb2f4141ef739f24f58cc3389ac8ab
dioh0922 <43507423+dioh0922@users.noreply.github.com>
2019-12-30 15:03:32 +0900

    結果ディレクトリの一覧を返すように追加

--- search_img_crawler.php
+++ search_img_crawler.php
@@ -1,11 +1,20 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
 
+	/*
 	$full = "python3 crawler.cgi ".$_POST["search_word"];
 	exec($full, $out);
 
 	foreach($out as $iter){
 		echo $iter;
+	}
+	*/
+
+	$dir = "get_result/";
+
+	$f_list = glob($dir."*");
+	foreach ($f_list as $iter) {
+		echo $iter."<br>";
 	}
 
 	//ディレクトリを全部見て、画像のパスを\n区切りで返す
